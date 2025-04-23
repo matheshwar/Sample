733bbf775f67a5006f0b24bcffe6906bce41518c
matheshwar <matheshwar.srinivasan@gmail.com>
2025-04-23 18:37:31 +0530

    bb

--- backend/index.js
+++ backend/index.js
@@ -7,5 +7,5 @@ app.get('/api/message', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Backend running on http://localhost:${PORT}`);
+    console.log(`Backend running on http://localhost:5000`);
 });
