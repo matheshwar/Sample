717e56689de6961413ad47f5b86ea1cdcefc3704
matheshwar <matheshwar.srinivasan@gmail.com>
2025-04-24 11:01:37 +0530

    name

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/api/message', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
+    res.json({ message: 'Hello from the UlaMagilan!' });
 });
 
 app.listen(PORT, () => {
