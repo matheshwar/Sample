0711b364e489325a6c33d869eff8c2b74208a18a
matheshwar <matheshwar.srinivasan@gmail.com>
2025-04-25 14:29:50 +0530

    https

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/api/message', (req, res) => {
-    res.json({ message: 'Matheshwar shini will achieve biggggg!!!!' });
+    res.json({ message: 'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice, and most of all, love of what you are doing or learning to do!' });
 });
 
 app.listen(PORT, () => {
